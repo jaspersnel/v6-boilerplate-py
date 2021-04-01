e75a710bd90395e4393249f93ee6c5318e86d974
Jasper Snel <me@jaspersnel.me>
2021-04-01 12:24:17 +0200

    Remove running of initial nodes

--- test.py
+++ test.py
@@ -7,14 +7,6 @@ organizations = client.get_organizations_in_my_collaboration()
 print(organizations)
 ids = [organization["id"] for organization in organizations]
 
-# Manually run the method at all the organization
-task = client.create_new_task({"method":"some_example_method"}, ids)
-print(task)
-
-# Get the results
-results = client.get_results(task.get("id"))
-print(results)
-
 # Let one organization take care of running the method at all organizations (and combining the results)
 master_task = client.create_new_task({"master": 1, "method":"master"}, [ids[0]])
 results = client.get_results(master_task.get("id"))
